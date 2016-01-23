ae00ae0a3a70b725a2c4bfcdb055f92ad6c05ea9
antronik7 <antoineberthiaumedutil@gmail.com>
2016-01-23 15:49:20 -0500

    Cree le score (si vous voulez lutiliser vous mettez le hud canvas et ajouter une valeur dans VOTRE script avec ScoreManager.score)

---
+++ DoorMonsterLoot/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static int score = 0;
+
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "" + score;
+	}
+}
---
+++ DoorMonsterLoot/Assets/Scripts/TestMiniJeu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMiniJeu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        ScoreManager.score += 100;
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DoorSelection");
+        //Application.LoadLevel(nomScene);
+    }
+}
