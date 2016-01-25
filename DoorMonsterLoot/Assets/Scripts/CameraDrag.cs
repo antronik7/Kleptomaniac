4db0e1d9561ff19b3322b0a16e2a658caa3e3677
antronik7 <antoineberthiaumedutil@gmail.com>
2016-01-25 00:37:51 -0500

    Jai fait le menu de selection du donjon avec le dragging

---
+++ DoorMonsterLoot/Assets/Scripts/CameraDrag.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDrag : MonoBehaviour {
+
+    private Vector2 previousPosition;
+    private Vector2 distanceBetWeenDrag;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            previousPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
+    }
+ 
+    void OnMouseDrag()
+    {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        distanceBetWeenDrag = objPosition - previousPosition;
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x + distanceBetWeenDrag.x, -15, 2), transform.position.y);
+
+        previousPosition = objPosition;
+    }
+}
---
+++ DoorMonsterLoot/Assets/Scripts/CastleController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CastleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+        //Application.LoadLevel(nomScene);
+    }
+}
