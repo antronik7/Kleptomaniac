892a115feb7c13bd84a64115737af409740b5963
unknown <antoineberthiaumedutil@gmail.com>
2016-04-05 18:21:36 -0400

    commencer le skull

--- DoorMonsterLoot/Assets/Scripts/CameraController.cs
+++ DoorMonsterLoot/Assets/Scripts/CameraController.cs
@@ -14,9 +14,9 @@ public class CameraController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x - thePlayer.transform.position.x < 5.5)
+        /*if(transform.position.x - thePlayer.transform.position.x < 5.5)
         {
             transform.position = new Vector3(thePlayer.transform.position.x + 5.5f, transform.position.y, transform.position.z);
-        }
+        }*/
     }
 }
