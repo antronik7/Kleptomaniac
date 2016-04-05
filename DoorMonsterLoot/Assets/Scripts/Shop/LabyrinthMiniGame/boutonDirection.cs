55e5dfc2a344cdc9f7e1d7557e73589caabbe6a4
xdsalut <cristianlavoie111@gmail.com>
2016-04-05 18:19:49 -0400

    les boutons de labyrinthe

---
+++ DoorMonsterLoot/Assets/Scripts/Shop/LabyrinthMiniGame/boutonDirection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boutonDirection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Vector4(0.8f, 0.8f, 0.8f, 1);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.05f, gameObject.transform.position.z);
+    }
+
+    void OnMouseUp()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Vector4(1, 1, 1, 1);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.05f, gameObject.transform.position.z);
+    }
+}
